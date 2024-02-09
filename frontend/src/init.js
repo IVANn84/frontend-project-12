164a3c056f25ac8c1fb7df320f1f1c902b913d04
Ivan Khristenko <ivan.tihii1984@gmail.com>
2024-02-09 09:42:52 +0300

    fix init js

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -2,7 +2,7 @@ import { createInstance } from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import React from 'react';
 import { Provider } from 'react-redux';
-import io from 'socket.io-client';
+// import io from 'socket.io-client';
 import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import store from './slices/index.js';
 import resources from './locales/index.js';
@@ -24,13 +24,13 @@ const init = async () => {
   };
   await i18n.use(initReactI18next).init(options);
 
-  const socket = io();
+  // const socket = io();
 
   return (
     <RollbarProvider config={rollbarConfig}>
       <ErrorBoundary>
         <Provider store={store}>
-          <SocketProvider socket={socket}>
+          <SocketProvider>
             <ApiProvider>
               <FilterProvider>
                 <I18nextProvider i18n={i18n}>
